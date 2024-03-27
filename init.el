54caf2705cc8779c7ee4c7a954d997ad67316450
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-03-26 21:17:29 -0400

    Added missing copyright notice to license statement.

--- init.el
+++ init.el
@@ -1,19 +1,19 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; This file is part of bungusmacs.                                           ;;
-;;                                                                            ;;
-;; Bungusmacs is free software: you can redistribute it and/or modify it      ;;
-;; under the terms of the GNU General Public License as published by the Free ;;
-;; Software Foundation, either version 3 of the License, or (at your option)  ;;
-;; any later version.                                                         ;;
-;;                                                                            ;;
-;; Bungusmacs is distributed in the hope that it will be useful, but WITHOUT  ;;
-;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or      ;;
-;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for   ;;
-;; more details.                                                              ;;
-;;                                                                            ;;
-;; You should have received a copy of the GNU General Public License along    ;;
-;; with bungusmacs. If not, see <https://www.gnu.org/licenses/>.              ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This file is part of bungusmacs.
+;;
+;; Copyright (c) 2023-2024 ona-li-toki-e-jan-Epiphany-tawa-mi
+;;
+;; Bungusmacs is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; Bungusmacs is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+;; more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with bungusmacs. If not, see <https://www.gnu.org/licenses/>.
 
 ;; Bungusmacs is my personal emacs configuration.
 ;;
